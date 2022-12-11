9aa539545c6ce07e1e175ac0abf7f28ddd796b1e
19-40812-2 <19-40812-2@student.aiub.edu>
2022-12-11 12:49:50 +0600

    Seeding Added

--- DAL/Migrations/Configuration.cs
+++ DAL/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 ﻿namespace DAL.Migrations
 {
+    using DAL.EF.Model;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -18,6 +20,31 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+            string[] admins = new string[] { "Admin1", "Admin2", "Admin3" };
+            List<Admin> cts = new List<Admin>();
+            foreach (var item in admins)
+            {
+                cts.Add(new Admin
+                {
+                    Name = item
+                }); ;
+            }
+            context.Admins.AddOrUpdate(cts.ToArray());
+            List<AdminToken> tokens = new List<AdminToken>();
+            Random rand = new Random();
+            for (int i = 1; i <= 5; i++)
+            {
+                tokens.Add(new AdminToken()
+                {
+                    Id = i,
+                    Tkey = i,
+                    Creation = DateTime.Now,
+                    Expiration = DateTime.Now ,
+                    AdminID = rand.Next(1, 3)
+                });
+                ;
+            }
+            context.AdminTokens.AddOrUpdate(tokens.ToArray());
         }
     }
 }
